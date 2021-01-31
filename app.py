3f466c8c3c55c1f41cf4592b8342df0b7e94351a
echapelier <53702403+echapelier@users.noreply.github.com>
2021-01-31 22:50:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ partie1_md = '''
     Cette politique régionale vise à accroître la cohésion économique, sociale et territoriale entre régions européennes – c’est pourquoi elle est aussi appelée politique de cohésion – et à réduire leurs écarts de développement. Elle se décline en programmes opérationnels régionaux, interrégionaux et nationaux articulés autour de priorités d’investissement définies sur sept ans pour chaque territoire. Ces priorités se répartissent selon des axes thématiques et elles définissent les conditions d’éligibilité des opérations.
 
     La sélection des projets fait également intervenir des variables telles que la taille, la durée des projets, le montant d’aide ou le type de porteur de projet. La présente analyse des opérations cofinancées par le FEDER et le FSE en France au cours de la période 2014-2020 propose donc de répondre aux questions suivantes :
-    - Quel est le profil des opérations soutenues et quel niveau d’aide reçoivent-ils ?
+    - Quel est le profil des opérations soutenues et quel niveau d’aide reçoivent-elles ?
     - Qui sont les bénéficiaires des fonds et comment se répartissent-ils ?
     - Observe-t-on des différences entre régions dans la mise en œuvre des fonds ?
     - Comment s’articulent le cycle de programmation de sept ans et le calendrier de réalisation des opérations ?
@@ -64,13 +64,15 @@ partie1_md = '''
     En procédant à l’analyse de données agrégées plutôt que par programme et par axe thématique, cette rétrospective offre aux gestionnaires, bénéficiaires et praticiens des fonds européens un éclairage renouvelé sur l’utilisation du FEDER et du FSE en France.
 '''
 
-partie2_md = '''
+partie2_md_a = '''
     La distribution des opérations en fonction des montants d’aides européennes reçues fait apparaître deux catégories de projets :
 
     - d’une part des projets **« courants »**, qui représentent 90% des opérations FEDER et FSE et représentent 30% des montants programmés ; 
     - d’autre part des projets **« d’ampleur »**, beaucoup moins nombreux (10% des opérations) mais qui absorbent la grande majorité des ressources (70% des fonds FEDER et FSE).
+'''
 
-    Dans le cas du FSE, le seuil d’entrée dans la catégorie des projets « d’ampleur » (séparant le neuvième et dixième décile) se situe à 326 000 euros tandis qu’il se situe à 700 000 euros pour le FEDER. En effet, les opérations FSE, qui sont liées à l’emploi, la formation et l’inclusion, tendent à être plus courtes (64% des projets durent moins d’un an) et affichent un coût total moyen de 447 000 euros ainsi qu’un niveau de cofinancement européen de 53%. En revanche, les opérations FEDER, généralement liées aux infrastructures et équipements, ont des durées plus longues (54% des projets durent deux à trois ans) et un coût plus élevé (960 000 euros en moyenne) tout en présentant un taux de cofinancement plus faible (40% en moyenne).
+partie2_md_b = '''
+    Dans le cas du FSE, le seuil d’entrée dans la catégorie des projets « d’ampleur » (séparant les neuvième et dixième déciles) se situe à 326 000 euros tandis qu’il se situe à 700 000 euros pour le FEDER. En effet, les opérations FSE, qui sont liées à l’emploi, la formation et l’inclusion, tendent à être plus courtes (64% des projets durent moins d’un an) et affichent un coût total moyen de 447 000 euros ainsi qu’un niveau de cofinancement européen de 53%. En revanche, les opérations FEDER, généralement liées aux infrastructures et équipements, ont des durées plus longues (54% des projets durent deux à trois ans) et un coût plus élevé (960 000 euros en moyenne) tout en présentant un taux de cofinancement plus faible (40% en moyenne).
 '''
 
 partie3_md = '''
@@ -90,7 +92,7 @@ partie4_md = '''
 
     Les organismes de formation sont également de nature différente suivant le profil des opérations : il s’agit principalement des GRETA, CFA et GIP de formation pour les projets « courants » et des OPCA de formation professionnelle pour les projets « d’ampleur ».
 
-    Concernant le FEDER, la catégorie des projets « d’ampleur » (avec une contribution européenne supérieure à 700 000 euros) marque une plus forte présence des collectivités territoriales et de leurs groupements : secteur communal (communes, intercommunalités, syndicats mixtes), départements et régions pour les actions d’aménagement du territoire.
+    Concernant le FEDER, la catégorie des projets « d’ampleur » (avec une contribution européenne supérieure à 700 000 euros) révèle une plus forte présence des collectivités territoriales et de leurs groupements : secteur communal (communes, intercommunalités, syndicats mixtes), départements et régions pour les actions d’aménagement du territoire.
 '''
 
 partie5_md = '''
@@ -124,13 +126,13 @@ partie6_md = '''
 
     Dans les cinq régions et départements d’outre-mer, régions dites « moins développées », l’État et les établissements publics portent une part significative des cofinancements européens (respectivement 8 et 10%, contre 0 et 3% en métropole). Dans le cas de l’État, il s’agit d’une part des mesures d’assistance technique à la gestion des fonds européens, et de l’autre des actions relatives au Service militaire adapté (SMA), un dispositif d’insertion destiné aux jeunes. De leur côté, les établissements publics mettent en œuvre des mesures relatives à l’emploi et à l’inclusion de plus grande ampleur qu’en métropole, ainsi que d’importantes opérations d’aménagement concernant notamment les infrastructures portuaires et le logement.
     
-    Les entreprises bénéficient également en outre-mer de davantage de financements européens, en raison de l’existence de mesures de compensation des surcoûts de l’ultrapériphéricité. En revanche, les conseils régionaux et établissements d’enseignement supérieur et de recherche occupent une place moindre que dans les régions métropolitaines dites « en transition » et « plus développées ».
+    Les entreprises en outre-mer bénéficient également de davantage de financements européens, en raison de l’existence de mesures de compensation des surcoûts de l’ultrapériphéricité. En revanche, les conseils régionaux et établissements d’enseignement supérieur et de recherche occupent une place moindre que dans les régions métropolitaines dites « en transition » et « plus développées ».
 '''
 
 partie7_md = '''
     ## 7. Des stratégies d’emploi des fonds différentes selon les régions
 
-    Les stratégies de sélection des opérations par les Conseils régionaux, autorités de gestion de la plupart des programmes opérationnels, s’est traduite par un ciblage des investissements qui a pu bénéficier à certaines catégories de bénéficiaires.
+    Les stratégies de sélection des opérations par les Conseils régionaux, autorités de gestion de la plupart des programmes opérationnels, se sont traduites par un ciblage des investissements qui a pu bénéficier à certaines catégories de bénéficiaires.
 
     Ainsi, deux régions se distinguent par une concentration relativement élevée des fonds : l’Île-de-France, où le Conseil régional reçoit près de la moitié des cofinancements et notamment pour la mise en place d’instruments financiers, et la Bretagne, où les établissements de recherche et d’enseignement supérieur portent une large part (47%) des projets « courants » tandis que le FSE y a été mis en œuvre par des contrats de formation professionnelle de grande ampleur.
 
@@ -140,7 +142,7 @@ partie7_md = '''
 partie8_md = '''
     ## 8. Les opérations de très grande envergure
 
-    Hors instruments financiers, une centaine d’opérations présente une contribution européenne supérieure à 10 millions d’euros. Elles représentent 0,3% des opérations mais 19,4% des contributions FEDER et FSE.
+    Hors instruments financiers, une centaine d’opérations présentent une contribution européenne supérieure à 10 millions d’euros. Elles représentent 0,3% des opérations mais 19,4% des contributions FEDER et FSE.
 
     Sont concernés en premier lieu les contrats de formation professionnelle portés par les Conseils régionaux et, dans une moindre mesure, certains organismes de formation et d’accompagnement des jeunes. Viennent ensuite les opérations relatives à l’emploi avec la mise en place de la Garantie Jeunes, les dispositifs d’accompagnement de Pôle Emploi et les aides à la mobilité.
     
@@ -150,7 +152,7 @@ partie8_md = '''
 partie9_md = '''
     ## 9. Les instruments financiers
 
-    **Une partie des financements FEDER a été allouée à la mise en place d’une soixantaine d’instruments financiers, c’est-à-dire des dispositifs d’aide remboursable représentant un total de plus de 354 millions d’euros.**
+    **Une partie des financements FEDER ont été alloués à la mise en place d’une soixantaine d’instruments financiers, c’est-à-dire des dispositifs d’aides remboursables représentant un total de plus de 354 millions d’euros.**
 
     Ces instruments sont principalement à destination des PME, et notamment des entreprises en création et/ou innovantes. Ils comprennent des instruments de participation (intervention en fonds propres et quasi-fonds propres), de prêt et de garantie, ainsi que des dotations à des fonds de fonds permettant d’abonder plusieurs instruments financiers. Ils sont essentiellement gérés par les Conseils régionaux (73%) et BPI France (18%).
 '''
@@ -160,22 +162,50 @@ partie10_md = '''
 
     **La réalisation des opérations a connu une montée en puissance progressive avec un pic des contributions européennes à la mi-parcours, c’est-à-dire fin 2017.**
 
-    Cependant, les opérations FEDER et FSE ont suivi deux tendances distinctes : après une brusque augmentation des contributions européennes en 2015, le FSE a conservé de 2016 à 2019 un rythme stable de réalisation des opérations ; la réalisation des opérations FEDER a quant à elle connu une croissance sensible et régulière jusqu’en 2017, avant de se stabiliser en 2018 et 2019. Plus de la moitié (55%) des opérations FEDER et FSE a démarré avant la fin 2016 et 89% avant la fin 2018, avec un pic de mise en œuvre en 2017. Ce pic a été suivi d’un rapide recul de la mise en œuvre des opérations de courte durée, en majorité cofinancées par le FSE (dès 2016 pour les opérations moins de 24 mois et à partir de 2017 pour les opérations de moins de 12 mois).
+    Cependant, les opérations FEDER et FSE ont suivi deux tendances distinctes : après une brusque augmentation des contributions européennes en 2015, le FSE a conservé de 2016 à 2019 un rythme stable de réalisation des opérations ; la réalisation des opérations FEDER a quant à elle connu une croissance sensible et régulière jusqu’en 2017, avant de se stabiliser en 2018 et 2019. Plus de la moitié (55%) des opérations FEDER et FSE ont démarré avant la fin 2016 et 89% avant la fin 2018, avec un pic de mise en œuvre en 2017. Ce pic a été suivi d’un rapide recul de la mise en œuvre des opérations de courte durée, en majorité cofinancées par le FSE (dès 2016 pour les opérations de moins de 24 mois et à partir de 2017 pour les opérations de moins de 12 mois).
 '''
 
 partie11_md = '''
     ## 11. Note méthodologique
 
-    L’analyse repose sur les données relatives à 38 532 opérations menées dans le cadre de 38 Programmes Opérationnels FEDER, FSE et IEJ 2014-2020. Elles ont été extraites le 01/12/2020 et sont issues des listes d’opérations programmées publiées au 29/07/2020 par l’Agence Nationale de la Cohésion des Territoires (16 411 opérations), au 01/12/2020 par la Région Nouvelle-Aquitaine (2 292 opérations), au 06/07/2020 par la Région Normandie (864 opérations), au 30/06/2020 par la Région Bretagne (529 opérations) et au 30/07/2020 par la Délégation générale à l’emploi et à la formation professionnelle (18 436 opérations). Ne sont donc pas incluses les opérations cofinancées au titre des programmes de coopération territoriale européenne (INTERREG). Les montants considérés représentent 83,2% des fonds FEDER et FSE-IEJ alloués à la France pour 2014-2020, la période de programmation n’étant pas clôturée à la date de l’analyse et les jeux de données publiés d’ayant donc pas un caractère définitif.
+    L’analyse repose sur les données relatives à 38 532 opérations menées dans le cadre de 38 Programmes opérationnels FEDER, FSE et IEJ 2014-2020. Elles ont été extraites le 01/12/2020 et sont issues des listes d’opérations programmées publiées au 29/07/2020 par l’Agence nationale de la cohésion des territoires (16 411 opérations), au 01/12/2020 par la Région Nouvelle-Aquitaine (2 292 opérations), au 06/07/2020 par la Région Normandie (864 opérations), au 30/06/2020 par la Région Bretagne (529 opérations) et au 30/07/2020 par la Délégation générale à l’emploi et à la formation professionnelle (18 436 opérations). Ne sont donc pas incluses les opérations cofinancées au titre des programmes de coopération territoriale européenne (INTERREG). Les montants considérés représentent 83,2% des fonds FEDER et FSE-IEJ alloués à la France pour 2014-2020, la période de programmation n’étant pas clôturée à la date de l’analyse et les jeux de données publiés d’ayant donc pas un caractère définitif.
 '''
 
 apropos_md = '''
     ## À propos
 
-    Romain SU, https://romain.su
-    
-    Elie HERBERICHS, eherberichs@novi-advisory.eu
+    Auteurs : [Elie HERBERICHS](eherberichs@novi-advisory.eu) et [Romain SU](https://romain.su) 
 '''
+
+lignes_feder = np.arange(1, data.loc[data['Fonds'] == 'FEDER', 'Montant UE programmé'].count())
+lignes_fse = np.arange(1, data.loc[data['Fonds'] == 'FSE', 'Montant UE programmé'].count())
+feder_normalises = (lignes_feder - lignes_feder.min()) / (lignes_feder.max() - lignes_feder.min()) 
+fse_normalises = (lignes_fse - lignes_fse.min()) / (lignes_fse.max() - lignes_fse.min())
+
+montants_feder = np.cumsum(data.loc[data['Fonds'] == 'FEDER', 'Montant UE programmé'].sort_values())
+montants_fse = np.cumsum(data.loc[data['Fonds'] == 'FSE', 'Montant UE programmé'].sort_values())
+montants_feder_normalises = (montants_feder - montants_feder.min()) / (montants_feder.max() - montants_feder.min()) 
+montants_fse_normalises = (montants_fse - montants_fse.min()) / (montants_fse.max() - montants_fse.min()) 
+
+fig1 = go.Figure()
+fig1.add_trace(go.Scatter(x=feder_normalises, y=montants_feder_normalises, name='FEDER', hoverinfo='none'))
+fig1.add_trace(go.Scatter(x=fse_normalises, y=montants_fse_normalises, name='FSE', hoverinfo='none'))
+fig1.update_layout(xaxis = dict(
+                                title_text = "Nombre d'opérations",
+                                tickmode = 'array',
+                                tickvals = np.arange(0.1, 1, 0.1),
+                                tickformat = '%'
+                                ),
+                    yaxis = dict(
+                                title_text = 'Montants UE programmés',
+                                tickmode = 'array',
+                                tickvals = np.arange(0.1, 1, 0.1),
+                                tickformat = '%'
+                                ),
+                    margin = dict(l=0, r=0, t=0, b=70),
+                    legend = dict(orientation="h", yanchor="top", y=1, xanchor="left", x=0.25),
+                    dragmode=False
+                    )
 
 data_pivot_sansindetermine = data_pivot.drop(index=['Indéterminé'])
 
@@ -205,6 +235,32 @@ for catbeneficiaire in data_pivot_sansindetermine.columns:
                   )
 
 fig2.update_layout(legend=dict(orientation="h", font=dict(size=12)), font=graph_fontstyle)
+
+data_sansindetermine = data[data['catbeneficiaire'] != 'Bénéficiaires de type indéterminé']
+data_feder_p1 = data_sansindetermine.loc[(data_sansindetermine['Fonds'] == 'FEDER') & (data_sansindetermine['Palier'] == 'P1'), 'catbeneficiaire'].value_counts(normalize=True, dropna=False).sort_index()
+data_feder_p2 = data_sansindetermine.loc[(data_sansindetermine['Fonds'] == 'FEDER') & (data_sansindetermine['Palier'] == 'P2'), 'catbeneficiaire'].value_counts(normalize=True, dropna=False).sort_index()
+data_fse_p1 = data_sansindetermine.loc[(data_sansindetermine['Fonds'] == 'FSE') & (data_sansindetermine['Palier'] == 'P1'), 'catbeneficiaire'].value_counts(normalize=True, dropna=False).sort_index()
+data_fse_p2 = data_sansindetermine.loc[(data_sansindetermine['Fonds'] == 'FSE') & (data_sansindetermine['Palier'] == 'P2'), 'catbeneficiaire'].value_counts(normalize=True, dropna=False).sort_index()
+
+fig4a = go.Figure(data=[
+    go.Bar(name='Nombre de projets « courants »', orientation='h', x=data_feder_p1, y=data_feder_p1.index, hoverinfo='none'),
+    go.Bar(name='Nombre de projets « d’ampleur »', orientation='h', x=data_feder_p2, y=data_feder_p2.index, hoverinfo='none')
+])
+fig4a.update_layout(barmode='stack',
+                    xaxis=dict(tickformat='%'),
+                    margin = dict(l=0, r=0, t=10, b=40),
+                    legend = dict(orientation="h", yanchor="bottom", y=-0.2, xanchor="right", x=0.78),
+                    dragmode=False)
+
+fig4b = go.Figure(data=[
+    go.Bar(name='Nombre de projets « courants »', orientation='h', x=data_fse_p1, y=data_fse_p1.index, hoverinfo='none'),
+    go.Bar(name='Nombre de projets « d’ampleur »', orientation='h', x=data_fse_p2, y=data_fse_p2.index, hoverinfo='none')
+])
+fig4b.update_layout(barmode='stack',
+                    xaxis=dict(tickformat='%'),
+                    margin = dict(l=0, r=0, t=10, b=40),
+                    legend = dict(orientation="h", yanchor="bottom", y=-0.2, xanchor="right", x=0.78),
+                    dragmode=False)
 
 fig5 = go.Figure(layout = dict(
                                polar = dict(
@@ -282,8 +338,15 @@ app.layout = html.Div(children=[
 
     html.Div([
         html.H2('2. Typologie des opérations'),
-        dcc.Markdown(children=partie2_md)
-    ], className="paragraph"),
+        html.Div([dcc.Markdown(children=partie2_md_a)], className="five columns"),
+        html.Div([
+            dcc.Graph(
+            id='repartition_montants',
+            figure=fig1,
+            config={'displayModeBar': False}
+        )], className="one-half column"),
+        html.Div([dcc.Markdown(children=partie2_md_b)], className="twelve columns"),
+    ], className="row"),
 
     html.Div([
         dcc.Markdown(children=partie3_md)
@@ -302,6 +365,26 @@ app.layout = html.Div(children=[
     html.Div([
         dcc.Markdown(children=partie4_md)
     ], className="paragraph"),
+
+    html.Figure([
+        html.Figcaption('Répartition des opérations FEDER par catégorie de bénéficiaire et de projet'),
+        dcc.Graph(
+            id='paliers_operations_feder',
+            figure=fig4a,
+            config={'displayModeBar': False},
+            style={'width':'90%'}
+        )
+    ]),
+
+    html.Figure([
+        html.Figcaption('Répartition des opérations FSE par catégorie de bénéficiaire et de projet'),
+        dcc.Graph(
+            id='paliers_operations_fse',
+            figure=fig4b,
+            config={'displayModeBar': False},
+            style={'width':'90%'}
+        )
+    ]),
 
     html.Div([
         dcc.Markdown(children=partie5_md)
